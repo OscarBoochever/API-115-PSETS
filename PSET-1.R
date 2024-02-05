1839a54359b749978da385984d4952505fe3ace9
Oscar Boochever <obooch12@gmail.com>
2024-02-04 21:15:08 -0500

    finished coding required

--- PSET-1.R
+++ PSET-1.R
@@ -46,6 +46,54 @@ print(t_test_result)
 
 
 # 18. Reproduce Estimates ####
-felm(realfinalprofit ~ ) 
+table3_model <- felm(realfinalprofit ~ atreatcash + atreatequip | groupnum + wave | 0 | sheno, data) 
+
+## Create trimmed data ====
+trimmed <- data %>% filter(is.na(trimgroup))
+
+## Create column two model ====
+table3_model_trimmed <- felm(realfinalprofit ~ atreatcash + atreatequip | groupnum + wave | 0 | sheno, trimmed) 
+
+## Print results ====
+tidy(table3_model)
+tidy(table3_model_trimmed)
+
+#trimgroup
+# quad group and wave fixed effects
+
+data <- data %>% mutate(wave_female = wave*female)
+
+# 19. Column 5 Row 3 onward ####
+table3_model_col5 <- felm(realfinalprofit ~ atreatcashfemale + atreatequipfemale + atreatcashmale + atreatequipmale | groupnum + wave + wave_female| 0 | sheno, data)
+coef_table <- tidy(table3_model_col5)
+
+# Define a vector of new names
+new_names <- c("Cash Treatment Female", "Equipment Treatment Female", "Cash Treatment Male", "Equipment Treatment Male")
+
+# Rename the terms in coef_table
+coef_table <- coef_table %>%
+  mutate(term = new_names)
+
+# Create the coefficient plot
+coefficient_plot <- ggplot(coef_table, aes(x = estimate, y = term, shape = term, color = term)) +
+  geom_point(size = 2.2) +
+  geom_errorbarh(aes(xmin = estimate - 1.96 * std.error, xmax = estimate + 1.96 * std.error), height = 0) +
+  geom_text(aes(label = paste(round(estimate, 2), " (", round(std.error, 2), ")", sep = "")), vjust = -1.2) + # Add text labels
+  scale_shape_manual(values = c("Cash Treatment Female" = 17, "Equipment Treatment Female" = 19, "Cash Treatment Male" = 17, "Equipment Treatment Male" = 19)) +
+  scale_color_manual(values = c("Cash Treatment Female" = "blue", "Equipment Treatment Female" = "blue", "Cash Treatment Male" = "darkorange", "Equipment Treatment Male" = "darkorange")) +
+  labs(x = "Coefficient", y = NULL, title = "Treatment Effects by Gender and Type") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) + # Center the title
+  guides(shape = FALSE, color = FALSE)  # Remove legend
+
+
+# Print the plot
+print(coefficient_plot)
+
+
+# redo 18 with firm level fixed effects
+
+#use ggplot2 
+
 
 
